3801955048a583c0f7fb6ccbd6837f7b7c2c802d
tenzin palgyal <tenzinpalgyal@tenzins-MacBook-Air.local>
2024-01-02 11:02:13 +0530

    Push from Pg

--- dlihe-project/src/App.jsx
+++ dlihe-project/src/App.jsx
@@ -1,48 +1,8 @@
-// import {Route,Routes} from 'react-router-dom'
 
-// import Navbar from './Pages/Navbar/Navbar'
-// import Home from './Pages/Home/Home'
-// import Lowerfoot from './Pages/Footers/Lowerfoot'
-// import Internship from './Pages/Internship /Internship'
-// import Contact from './Pages/Contact/Contact'
-// import Affiliation from './Pages/affiliation/Affiliation'
-// import Academic from './Pages/academic /Academic'
-// import About from './Pages/aboutdlihe/About'
-// import Admission from './Pages/Admission/Admission'
-// import {Schopage}  from './Pages/Scholarship/Schopage'
-// import Announce from './Pages/Announce/Announce'
-// import Student from './Pages/StudentCouncil/Student'
-
-// import './App.css'
-
-// function App() {
-//   return (
-//     <div>
-//       <Navbar/>
-//       <Routes>
-//         <Route path='/' element={<Home/>}/>
-//         <Route path='/student' element={<Student/>}/>
-//         <Route path='/announcement' element={<Announce/>}/>
-//         <Route path='/intern' element={<Internship/>}/>
-//         <Route path='/admission' element={<Admission/>}/>
-//         <Route path='/contact' element={<Contact/>}/>
-//         <Route path='/affiliation' element={<Affiliation/>}/>
-//         <Route path='/academic' element={<Academic/>}/>
-//         <Route path='/about' element={<About/>}/>
-//         <Route path='/scholarship' element={<Schopage/>}/>
-//         <Route path='/student' elemen={<Student/>}t/>
-//       </Routes>
-//       <Lowerfoot/>
-//     </div>
-//   )
-// }
-
-// export default App
 import { Route, Routes } from "react-router-dom";
 
 import Navbar from "./Pages/Navbar/Navbar";
 import Home from "./Pages/Home/Home";
-import Footer from "./Pages/Footers/Footer";
 import Internship from "./Pages/Internship /Internship";
 import Contact from "./Pages/Contact/Contact";
 import Affiliation from "./Pages/affiliation/Affiliation";
@@ -50,12 +10,14 @@ import Academic from "./Pages/academic /Academic";
 import About from "./Pages/aboutdlihe/About";
 import Admission from "./Pages/Admission/Admission";
 import { Schopage } from "./Pages/Scholarship/Schopage";
-import "./App.css";
 import News from "./Pages/News";
 import NewsDetail from "./Pages/NewsDetail";
 import EventDetail from "./Pages/EventDetail";
 import Announcement from "./Pages/AllAnouncement";
 import Student from "./Pages/StudentCouncil/Student";
+import Lowerfoot from "./Pages/Footers/Lowerfoot";
+
+import "./App.css";
 
 function App() {
   return (
@@ -77,7 +39,7 @@ function App() {
         <Route path="/event/:id" element={<EventDetail />} />
         <Route path="/student" elemen={<Student />} t />
       </Routes>
-      <Footer />
+      <Lowerfoot />
     </div>
   );
 }
